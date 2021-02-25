fe09dcf8cce84a8fb9f683105ab8870054206cdf
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-02-25 15:15:44 +0100

    add. js and css

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// definizione varibili
+
+  // definisco variabile nome
+  var nome = prompt('Inserisci il tuo nome');
+
+  // definisco variabile cognome
+  var cognome = prompt('Inserisci il tuo cognome');
+
+  // definisco variabile colore preferito
+  var colorePreferito = prompt('Scrivi il tuo colore preferito');
+
+  // inserisco esito in html
+  document.getElementById('newpassword').innerHTML = nome + cognome + colorePreferito + '27';
